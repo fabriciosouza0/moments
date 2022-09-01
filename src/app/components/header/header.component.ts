865be4f3d7c955be2611b2d33a77854ef4b6fe6c
Fabricio-derp <derp.fp@gmail.com>
2022-09-01 19:01:05 -0300

    Adding mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faBars = faBars;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  menu(event: Event) {
+    event.stopPropagation();
+    const navLinks = document.getElementsByClassName('nav-links')[0];
+
+    if (navLinks.classList.contains('show')) {
+      navLinks.classList.remove('show');
+    } else {
+      navLinks.classList.add('show');
+    }
   }
 
 }
